a766edd83d75b804e4bb9f693dc468e27c57ab26
rohitjadhav45 <rohitjadhav4510@gmail.com>
2025-07-03 16:34:51 +0530

    refactor: Improve code formatting and trim input values in LoginForm and RegisterForm

--- src/Forms/LoginForm.jsx
+++ src/Forms/LoginForm.jsx
@@ -1,11 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Alert, ScrollView } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  ScrollView,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { styles, themeStyles } from '../../assets/styles/ThemeStyles';
+import {styles, themeStyles} from '../../assets/styles/ThemeStyles';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import LinearGradient from 'react-native-linear-gradient';
 import Apiclient from '../utils/Apiclient';
-export const LoginForm = ({ onLogin, ShowloginForm, SigninWithApple, SigninWithFacebook, SigninWithGoogle, theme }) => {
+export const LoginForm = ({
+  onLogin,
+  ShowloginForm,
+  SigninWithApple,
+  SigninWithFacebook,
+  SigninWithGoogle,
+  theme,
+}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
@@ -18,11 +32,11 @@ export const LoginForm = ({ onLogin, ShowloginForm, SigninWithApple, SigninWithF
         return;
       }
       const parameter = {
-        username: email,
-        password: password,
-      }
+        username: email.trim(),
+        password: password.trim(),
+      };
 
-      const res = await Apiclient.post('/login', parameter)
+      const res = await Apiclient.post('/login', parameter);
       if (res.data.message === 'Login successful') {
         onLogin();
         console.log(res.data.user);
@@ -30,7 +44,7 @@ export const LoginForm = ({ onLogin, ShowloginForm, SigninWithApple, SigninWithF
 
         const userDataString = JSON.stringify(res.data.user);
         await AsyncStorage.setItem('UserData', userDataString);
-        Alert.alert('Success', `LogIn Success.`, [{ text: 'OK' }]);
+        Alert.alert('Success', `LogIn Success.`, [{text: 'OK'}]);
       }
     } catch (err) {
       console.log(err);
@@ -49,7 +63,6 @@ export const LoginForm = ({ onLogin, ShowloginForm, SigninWithApple, SigninWithF
         if (password !== null) {
           setPassword(password);
         }
-
       } catch (err) {
         console.warn('Error reading from AsyncStorage:', err);
       }
@@ -63,11 +76,22 @@ export const LoginForm = ({ onLogin, ShowloginForm, SigninWithApple, SigninWithF
   };
   return (
     <>
-      <ScrollView style={{ position: 'absolute', flex: 1, width: '100%', height: '100%', top: '20%' }}>
+      <ScrollView
+        style={{
+          position: 'absolute',
+          flex: 1,
+          width: '100%',
+          height: '100%',
+          top: '20%',
+        }}>
         <View style={[styles.formContainer, themeStyles[theme].formContainer]}>
-          <Text style={[styles.formTitle, themeStyles[theme].text]}>Sign In</Text>
-          <View style={[{ width: '100%', padding: '7' }]}>
-            <Text style={[styles.SingInlabel, themeStyles[theme].SingInlabel]}>User Name</Text>
+          <Text style={[styles.formTitle, themeStyles[theme].text]}>
+            Sign In
+          </Text>
+          <View style={[{width: '100%', padding: '7'}]}>
+            <Text style={[styles.SingInlabel, themeStyles[theme].SingInlabel]}>
+              User Name
+            </Text>
             <TextInput
               value={email}
               onChangeText={setEmail}
@@ -77,9 +101,11 @@ export const LoginForm = ({ onLogin, ShowloginForm, SigninWithApple, SigninWithF
               placeholderTextColor={themeStyles[theme].placeholder.color}
             />
           </View>
-          <View style={[{ width: '100%', padding: '7' }]}>
-            <Text style={[styles.SingInlabel, themeStyles[theme].SingInlabel]}>Password</Text>
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <View style={[{width: '100%', padding: '7'}]}>
+            <Text style={[styles.SingInlabel, themeStyles[theme].SingInlabel]}>
+              Password
+            </Text>
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
               <TextInput
                 value={password}
                 onChangeText={setPassword}
@@ -87,7 +113,9 @@ export const LoginForm = ({ onLogin, ShowloginForm, SigninWithApple, SigninWithF
                 secureTextEntry={!showPassword}
                 placeholderTextColor={themeStyles[theme].placeholder.color}
               />
-              <TouchableOpacity onPress={togglePasswordVisibility} style={{ padding: 10, position: 'absolute', right: 15, top: 10 }}>
+              <TouchableOpacity
+                onPress={togglePasswordVisibility}
+                style={{padding: 10, position: 'absolute', right: 15, top: 10}}>
                 <Icon
                   name={showPassword ? 'eye' : 'eye-slash'}
                   size={20}
@@ -97,32 +125,50 @@ export const LoginForm = ({ onLogin, ShowloginForm, SigninWithApple, SigninWithF
             </View>
           </View>
           <View style={styles.Loginerror}>
-            {error ? <Text style={[styles.error, themeStyles[theme].error]}>{error}</Text> : null}
+            {error ? (
+              <Text style={[styles.error, themeStyles[theme].error]}>
+                {error}
+              </Text>
+            ) : null}
           </View>
-          <TouchableOpacity style={[themeStyles[theme].button]} onPress={handleLogin}>
+          <TouchableOpacity
+            style={[themeStyles[theme].button]}
+            onPress={handleLogin}>
             <LinearGradient
               colors={['rgb(238, 41, 123)', 'rgb(183, 1, 255)']}
-              start={{ x: 1, y: 0 }}
-              end={{ x: 0, y: 1 }}
-              style={styles.button}
-            >
+              start={{x: 1, y: 0}}
+              end={{x: 0, y: 1}}
+              style={styles.button}>
               <Text style={styles.buttonText}>Sign In</Text>
             </LinearGradient>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => ShowloginForm()} style={{ alignItems: 'center', marginTop: 20 }}>
-            <Text style={{ color: theme === 'dark' ? '#fff' : 'blue', textDecorationLine: 'underline', fontSize: 16 }}>
+          <TouchableOpacity
+            onPress={() => ShowloginForm()}
+            style={{alignItems: 'center', marginTop: 20}}>
+            <Text
+              style={{
+                color: theme === 'dark' ? '#fff' : 'blue',
+                textDecorationLine: 'underline',
+                fontSize: 16,
+              }}>
               Don't have an account? Sign Up
             </Text>
           </TouchableOpacity>
-          <View style={[styles.Othersinginoption, { marginBottom: 80 }]}>
+          <View style={[styles.Othersinginoption, {marginBottom: 80}]}>
             <View style={styles.Loginoption}>
-              <TouchableOpacity style={[styles.Loginoptionbtn, styles.Applebtn]} onPress={() => SigninWithApple()}>
+              <TouchableOpacity
+                style={[styles.Loginoptionbtn, styles.Applebtn]}
+                onPress={() => SigninWithApple()}>
                 <Icon name="apple" size={24} color="#fff" />
               </TouchableOpacity>
-              <TouchableOpacity style={[styles.Loginoptionbtn, styles.Facebookbtn]} onPress={() => SigninWithFacebook()}>
+              <TouchableOpacity
+                style={[styles.Loginoptionbtn, styles.Facebookbtn]}
+                onPress={() => SigninWithFacebook()}>
                 <Icon name="facebook" size={24} color="#fff" />
               </TouchableOpacity>
-              <TouchableOpacity style={[styles.Loginoptionbtn, styles.Googlebtn]} onPress={() => SigninWithGoogle()}>
+              <TouchableOpacity
+                style={[styles.Loginoptionbtn, styles.Googlebtn]}
+                onPress={() => SigninWithGoogle()}>
                 <Icon name="google" size={24} color="#fff" />
               </TouchableOpacity>
             </View>
