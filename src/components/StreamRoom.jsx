9950bc58ec4333700be9d40c976775d1345f6e51
vikram760 <bhosalevikram760.com>
2025-07-22 13:07:51 +0530

    update StreamRoom.jsx

--- src/components/StreamRoom.jsx
+++ src/components/StreamRoom.jsx
@@ -682,7 +682,6 @@ const StreamRoom = ({
                                 end={{ x: 0.5, y: 0 }}
                                 style={styles.strRoomFooter}
                             >
-                                {!openMoreSettingList && (
                                     <>
                                         <View style={styles.strRoomFooterChatOrActionsBox}>
                                             <View style={[styles.streamChatContainer]}>
@@ -694,7 +693,7 @@ const StreamRoom = ({
                                                         <View key={ind} style={styles.streamChatItem}>
                                                             <Image style={styles.streamChatItemProfileImg} source={chat.userProfile} />
                                                             <View numberOfLines={1} style={styles.streamChatMessageBox}>
-                                                                <Text numberOfLines={1} style={[styles.streamChatUserName, { color: `${chat?.TYPE === "USERJOINED" ? `#00F6CD` : chat.TYPE === "USERLEFT" ? '#DC112C' : `#DEEE4F`}`, paddingTop: `${chat?.TYPE === "USERJOINED" ? `20` : `0`}` }]}>
+                                                                <Text numberOfLines={1} style={[styles.streamChatUserName, { color: `${chat?.TYPE === "USERJOINED" ? `#00F6CD` : chat.TYPE === "USERLEFT" ? '#DC112C' : `#DEEE4F`}`, paddingTop: `${chat?.TYPE === "USERJOINED" ? `0` : `0`}` }]}>
                                                                     {chat.userName?.length > 30 ? chat.userName?.slice(0, 30) + '...' : chat?.userName}
                                                                 </Text>
                                                                 <Text numberOfLines={2} style={styles.streamChatMessage}>
@@ -720,7 +719,6 @@ const StreamRoom = ({
                                             </View>
                                         </View>
                                     </>
-                                )}
                                 {openMoreSettingList && (
                                     <Animated.View
                                         style={[
